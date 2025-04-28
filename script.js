c9224ce54a0be5a208d992b85199df6456e6999d
LauraPereira2007 <165840254+LauraPereira2007@users.noreply.github.com>
2025-04-28 16:51:11 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarAlerta() {
+  alert("Você ama Arctic Monkeys tanto quanto nós!");
+  document.getElementById("mensagem").textContent = "Explore os álbuns: AM, Tranquility Base Hotel & Casino e mais!";
+}
